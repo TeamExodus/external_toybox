CyanogenMod/android_external_toybox

toybox: stat: add -t option for terse form output
Change-Id: Ia06006f65f6dcac87b9cc4fbfeee02cd0bf3ebd3

generated/flags.h
@@ -2198,13 +2198,14 @@
 #undef FLAG_a
 #endif
 
-// stat c:f c:f
+// stat ct:f ct:f
 #undef OPTSTR_stat
-#define OPTSTR_stat "c:f"
+#define OPTSTR_stat "ct:f"
 #ifdef CLEANUP_stat
 #undef CLEANUP_stat
 #undef FOR_stat
 #undef FLAG_f
+#undef FLAG_t
 #undef FLAG_c
 #endif
 
@@ -4697,7 +4698,8 @@
 #define TT this.stat
 #endif
 #define FLAG_f (1<<0)
-#define FLAG_c (1<<1)
+#define FLAG_t (1<<1)
+#define FLAG_c (1<<2)
 #endif
 
 #ifdef FOR_strings

generated/newtoys.h
@@ -202,7 +202,7 @@ USE_SKELETON_ALIAS(NEWTOY(skeleton_alias, "b#dq", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SLEEP(NEWTOY(sleep, "<1", TOYFLAG_BIN))
 USE_SORT(NEWTOY(sort, USE_SORT_FLOAT("g")USE_SORT_BIG("S:T:m" "o:k*t:xbMcszdfi") "run", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SPLIT(NEWTOY(split, ">2a#<1=2>9b#<1l#<1", TOYFLAG_USR|TOYFLAG_BIN))
-USE_STAT(NEWTOY(stat, "c:f", TOYFLAG_BIN)) 
+USE_STAT(NEWTOY(stat, "ct:f", TOYFLAG_BIN)) 
 USE_STRINGS(NEWTOY(strings, "an#=4<1fo", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SU(NEWTOY(su, "lmpc:s:", TOYFLAG_BIN|TOYFLAG_ROOTONLY))
 USE_SULOGIN(NEWTOY(sulogin, "t#<0=0", TOYFLAG_SBIN|TOYFLAG_NEEDROOT))

toys/other/stat.c
@@ -2,18 +2,19 @@
  * Copyright 2012 <warior.linux@gmail.com>
  * Copyright 2013 <anand.sinha85@gmail.com>
 
-USE_STAT(NEWTOY(stat, "c:f", TOYFLAG_BIN)) 
+USE_STAT(NEWTOY(stat, "ct:f", TOYFLAG_BIN)) 
 
 config STAT
   bool stat
   default y
   help
-    usage: stat [-f] [-c FORMAT] FILE...
+    usage: stat [-f] [-t] [-c FORMAT] FILE...
 
     Display status of files or filesystems.
 
     -f display filesystem status instead of file status
     -c Output specified FORMAT string instead of default
+    -t Display info in terse form
 
     The valid format escape sequences for files:
     %a  Access bits (octal) |%A  Access bits (flags)|%b  Blocks allocated
@@ -149,8 +150,10 @@ void stat_main(void)
       "Device: %Dh/%dd\t Inode: %i\t Links: %h\n"
       "Access: (%a/%A)\tUid: (%u/%U)\tGid: (%g/%G)\n"
       "Access: %x\nModify: %y\nChange: %z";
+  char *terse_format = "%n %s %b %f %u %g %D %i %h %t %T %X %Y %Z %o";
 
   if (toys.optflags & FLAG_c) format = TT.fmt;
+  if (toys.optflags & FLAG_t) format = terse_format;
 
   for (; *toys.optargs; toys.optargs++) {
     char *f;

TeamExodus/external_toybox

generated/flags.h
@@ -2198,13 +2198,14 @@
 #undef FLAG_a
 #endif
 
-// stat c:f c:f
+// stat c:tf c:tf
 #undef OPTSTR_stat
-#define OPTSTR_stat "c:f"
+#define OPTSTR_stat "c:tf"
 #ifdef CLEANUP_stat
 #undef CLEANUP_stat
 #undef FOR_stat
 #undef FLAG_f
+#undef FLAG_t
 #undef FLAG_c
 #endif
 
@@ -4697,7 +4698,8 @@
 #define TT this.stat
 #endif
 #define FLAG_f (1<<0)
-#define FLAG_c (1<<1)
+#define FLAG_t (1<<1)
+#define FLAG_c (1<<2)
 #endif
 
 #ifdef FOR_strings

generated/newtoys.h
@@ -202,7 +202,7 @@ USE_SKELETON_ALIAS(NEWTOY(skeleton_alias, "b#dq", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SLEEP(NEWTOY(sleep, "<1", TOYFLAG_BIN))
 USE_SORT(NEWTOY(sort, USE_SORT_FLOAT("g")USE_SORT_BIG("S:T:m" "o:k*t:xbMcszdfi") "run", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SPLIT(NEWTOY(split, ">2a#<1=2>9b#<1l#<1", TOYFLAG_USR|TOYFLAG_BIN))
-USE_STAT(NEWTOY(stat, "c:f", TOYFLAG_BIN)) 
+USE_STAT(NEWTOY(stat, "c:tf", TOYFLAG_BIN)) 
 USE_STRINGS(NEWTOY(strings, "an#=4<1fo", TOYFLAG_USR|TOYFLAG_BIN))
 USE_SU(NEWTOY(su, "lmpc:s:", TOYFLAG_BIN|TOYFLAG_ROOTONLY))
 USE_SULOGIN(NEWTOY(sulogin, "t#<0=0", TOYFLAG_SBIN|TOYFLAG_NEEDROOT))

toys/other/stat.c
@@ -2,18 +2,19 @@
  * Copyright 2012 <warior.linux@gmail.com>
  * Copyright 2013 <anand.sinha85@gmail.com>
 
-USE_STAT(NEWTOY(stat, "c:f", TOYFLAG_BIN)) 
+USE_STAT(NEWTOY(stat, "c:tf", TOYFLAG_BIN)) 
 
 config STAT
   bool stat
   default y
   help
-    usage: stat [-f] [-c FORMAT] FILE...
+    usage: stat [-f] [-t] [-c FORMAT] FILE...
 
     Display status of files or filesystems.
 
     -f display filesystem status instead of file status
     -c Output specified FORMAT string instead of default
+    -t Display info in terse form
 
     The valid format escape sequences for files:
     %a  Access bits (octal) |%A  Access bits (flags)|%b  Blocks allocated
@@ -149,8 +150,10 @@ void stat_main(void)
       "Device: %Dh/%dd\t Inode: %i\t Links: %h\n"
       "Access: (%a/%A)\tUid: (%u/%U)\tGid: (%g/%G)\n"
       "Access: %x\nModify: %y\nChange: %z";
+  char *terse_format = "%n %s %b %f %u %g %D %i %h %t %T %X %Y %Z %o";
 
   if (toys.optflags & FLAG_c) format = TT.fmt;
+  if (toys.optflags & FLAG_t) format = terse_format;
 
   for (; *toys.optargs; toys.optargs++) {
     char *f;